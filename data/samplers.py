af8e8ec392777ed747ed4cfeb3e0a229e5556f8c
msc5 <19colemanm@gmail.com>
2022-01-08 20:03:22 -0600

    watching the crown

--- data/samplers.py
+++ data/samplers.py
@@ -1,5 +1,6 @@
 import random
 import pprint
+import math
 
 
 def SimpleSampler(data, params=None):
@@ -32,8 +33,23 @@ def BatchSampler(data, params):
         elif full_permute:
             yield [*val, *firstval[len(val):]]
 
+
 def ParallelSampler(data, params):
-    pass
+    batch_size = params.get('batch_size', 1)
+    it_data = iter(data)
+    iters = [next(it_data) for _ in range(batch_size)]
+    while True:
+        vals = []
+        for i, it in enumerate(iters):
+            try:
+                vals.append(next(it))
+            except StopIteration:
+                try:
+                    iters[i] = next(it_data)
+                except StopIteration:
+                    return
+                vals.append(next(iters[i]))
+        yield vals
 
 
 def RandomBatchSampler(data, params):
@@ -45,14 +61,24 @@ def RandomBatchSampler(data, params):
 
 if __name__ == '__main__':
 
-    pp = pprint.PrettyPrinter()
+    pp = pprint.PrettyPrinter(width=200)
 
-    n = 30
-    bs = 4
-    data = [f'{i:3.0f}' for i in range(n)]
-    sampler = RandomBatchSampler(data, {'batch_size': bs})
+    m = (5, 20)
+    data = [
+        [
+            f'{j - m[0] + 1:3.0f}_{i:<3.0f}' for i in range(j)
+        ] for j in range(*m)
+    ]
+    pp.pprint([[a for a in b] for b in data])
+    sampler = ParallelSampler(iter(iter(d)
+                              for d in data), {'batch_size': m[0]})
 
-    print(data)
     for i, x in enumerate(sampler):
-        pp.pprint(x)
-    print('Permuted: ', 100 * ((i + 1) * bs) / n, '%')
+        print(f'{i:3}', x)
+    n_data = (m[1] - m[0]) * (m[0] + math.ceil(m[0] / 2))
+    n_seen = (i + 1) * m[0]
+    n_norm = m[0]**2
+    print('# Data: ', n_data)
+    print('# Seen: ', n_seen)
+    print('# Norm: ', n_norm)
+    print('Permuted: ', n_seen / n_data, '%')
